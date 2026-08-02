39122900e9620d563b09a66301cd288187ab96ea
Origamini <kukavitx@gmail.com>
2026-08-02 14:09:06 +0530

    eddited the output.tf file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,8 @@
 output "storage_bucket_name" {
   description = "Name of the Cloud Storage Bucket"
-  value = google_storage_bucket.raw_landing.name
+  value       = google_storage_bucket.raw_landing.name
 }
 output "bigquery_dataset_id" {
-    description = "BigQuery Dataset ID"
-    value = google_bigquery_dataset.staging_dataset.dataset_id
+  description = "BigQuery Dataset ID"
+  value       = google_bigquery_dataset.staging_dataset.dataset_id
 }
